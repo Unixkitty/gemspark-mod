8485f5cd3da01c1fa05d1af52c72ce3db0a4174d
Unixkitty <uni@unixkitty.com>
2021-09-06 17:00:44 +0300

    Remove unnecessary renderLabels() override in PedestalScreen

--- src/main/java/com/unixkitty/gemspark/client/gui/PedestalScreen.java
+++ src/main/java/com/unixkitty/gemspark/client/gui/PedestalScreen.java
@@ -4,10 +4,8 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.unixkitty.gemspark.Gemspark;
 import com.unixkitty.gemspark.container.ContainerPedestal;
-import com.unixkitty.gemspark.init.ModBlocks;
 import com.unixkitty.gemspork.lib.HelperUtil;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
@@ -22,6 +20,14 @@ public class PedestalScreen extends ContainerScreen<ContainerPedestal>
     }
 
     @Override
+    protected void init()
+    {
+        super.init();
+
+        this.titleLabelX = (this.imageWidth - this.font.width(this.title)) / 2;
+    }
+
+    @Override
     public void render(MatrixStack matrixStack, final int mouseX, final int mouseY, final float partialTicks)
     {
         this.renderBackground(matrixStack);
@@ -33,16 +39,7 @@ public class PedestalScreen extends ContainerScreen<ContainerPedestal>
     protected void renderBg(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY)
     {
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        getMinecraft().getTextureManager().bind(BACKGROUND_TEXTURE);
-
+        this.getMinecraft().getTextureManager().bind(BACKGROUND_TEXTURE);
         this.blit(matrixStack, (width - imageWidth) / 2, (height - imageHeight) / 2, 0, 0, imageWidth, imageHeight);
-    }
-
-    @Override
-    protected void renderLabels(MatrixStack matrixStack, int mouseX, int mouseY)
-    {
-        String s = I18n.get("text.pedestal.title");
-        this.font.draw(matrixStack, s, (float) (this.imageWidth / 2 - this.font.width(s) / 2), 6.0F, 0x404040);
-        this.font.draw(matrixStack, I18n.get("container.inventory"), 8.0F, (float) (this.imageHeight - 96 + 2), 0x404040);
     }
 }
